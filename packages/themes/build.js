7e369924cbdcee79efca758b045c10a4cf5eb853
littleduck <littleduck1219@gmail.com>
2024-07-08 23:03:15 +0900

    package & test

---
+++ packages/themes/build.js
@@ -0,0 +1,37 @@
+import esbuild from 'esbuild';
+import pkg from './package.json' assert {type: 'json'}
+
+const dev = process.argv.includes('--dev');
+const minify = !dev;
+const watch = process.argv.includes('--watch');
+
+const external = Object.keys({
+    ...pkg.dependencies,
+    ...pkg.peerDependencies,
+})
+
+const baseConfig = {
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outdir: 'dist',
+    target: "es2019",
+    watch, // 17부터는 if(watch) 로 사용
+    external,
+}
+
+Promise.all([
+    esbuild.build({
+        ...baseConfig,
+        format: 'esm',
+    }),
+    esbuild.build({
+        ...baseConfig,
+        format: 'cjs',
+        outExtension: { '.js': '.cjs' },
+    })
+]).catch(() => {
+    console.error('Build failed');
+    process.exit(1);
+})
