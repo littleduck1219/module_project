5535a9414dfdc2bd5682b43af9e17924cb198590
littleduck <littleduck1219@gmail.com>
2024-10-15 23:34:24 +0900

    Text components

--- packages/react/components/layout/src/block.css.ts
+++
@@ -1,8 +0,0 @@
-import { vars } from "@duck-ui/themes";
-import { style } from "@vanilla-extract/css";
-
-export const blockStyle = style({
-  width: "100px",
-  height: "100px",
-  backgroundColor: vars.colors.$scale.pink[500],
-});
---
+++ services/storybook/stories/ReactComponents/Layout/Text.stories.tsx
@@ -0,0 +1,17 @@
+import { Text as _Text } from "@duck-ui/react-components-layout";
+import "@duck-ui/react-components-layout/style.css";
+
+export default {
+  title: "React Components/Layout/Text",
+  component: _Text,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const TextStory = {
+  args: {
+    padding: 5,
+  },
+};
