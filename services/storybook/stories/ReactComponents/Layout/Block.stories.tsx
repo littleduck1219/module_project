0bd345a01dc80da0109a2713386b1ad7cc64e684
littleduck <107936957+littleduck1219@users.noreply.github.com>
2024-10-15 09:22:09 +0900

    dev mode
    watch mode, stroybook

---
+++ services/storybook/stories/ReactComponents/Layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import { Block as _Block } from "@duck-ui/react-components-layout";
+import "@duck-ui/react-components-layout/style.css";
+
+export default {
+  title: "React Components/Layout/Block",
+  component: _Block,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const BlockStory = {};
